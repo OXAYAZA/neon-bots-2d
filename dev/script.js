a0a493ec7971104b9bec98f3a6b8cc587154e8b0
oxayaza <gashishmarihuanovich@gmail.com>
2019-10-23 16:13:08 +0300

    SYS: day end

--- dev/script.js
+++ dev/script.js
@@ -1,23 +1,34 @@
+// Debug
+function debug ( data ) {
+	document.getElementById( 'debug' ).innerText = JSON.stringify( data, null, 2 );
+}
+
 // Main
 window.addEventListener( 'load', function () {
 	let canvas = new Canvas();
 
-	canvas.node.addEventListener( 'click', function ( event )  {
+	canvas.node.addEventListener( 'mousemove', ( event ) => {
 		canvas.add( new Unit({
-			x: event.clientX,
-			y: event.clientY,
+			x: canvas.rect.width / 2,
+			y: canvas.rect.height + 10,
 			destX: event.clientX,
 			destY: event.clientY,
 			canvas: canvas,
-			speed: ~~(Math.random() * 50) + 1
+			speed: ~~(Math.random() * 20)
 		}));
 	});
 
-	canvas.node.addEventListener( 'mousemove', function ( event )  {
-		for ( let id in canvas.objects ) {
-			let unit = canvas.objects[ id ];
-			unit.destX = event.clientX;
-			unit.destY = event.clientY;
-		}
-	});
+	setInterval( () => {
+		canvas.add( new Unit({
+			x: canvas.rect.width / 2,
+			y: canvas.rect.height + 10,
+			destX: Math.random() * canvas.rect.width,
+			destY: Math.random() * canvas.rect.height / 2,
+			canvas: canvas,
+		}));
+	}, 500 );
+
+	setInterval( () => {
+		debug( Object.keys( canvas.objects ).length );
+	}, 40 );
 });
